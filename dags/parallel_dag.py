19cf900dc76bab969e5a9a2a4e917be61ec7101a
Kimaru thagana <macbuntu@localhost.localdomain>
2021-06-05 11:22:31 +0300

    refactor bash command to use params

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -27,7 +27,7 @@ with DAG("PARALLEL_TASK_GROUP",
     
     task_1 = BashOperator(
         task_id = "task_1",
-        bash_command='sleep 3 && echo "slept at {{ task_id }}"'
+        bash_command='sleep 3 && echo "slept at beginning"'
     )
     # create a task group where tasks will be grouped executed
     with TaskGroup(group_id="processing_group") as processing_group:
@@ -35,27 +35,26 @@ with DAG("PARALLEL_TASK_GROUP",
         with TaskGroup(group_id="process_pool_1") as process_pool_1:
             p1_task_1 = BashOperator(
             task_id = "p1_task_1", # similar task id but since its in group, it becomes group_id.task_id
-            bash_command='sleep 3 && echo "slept within group at {{ task_id }}"'
+            params={'task_id':task_id},
+            bash_command='sleep 3 && echo "slept within group at {{ params.task_id }}"'
             )
             
             p1_task_2 = BashOperator(
             task_id = "p1_task_2",
-            bash_command='sleep 3 && echo "slept within group {{ group_id}} at {{ task_id }}"'
+            params={'group_id':processing_pool_1,'task_id':task_id},
+            bash_command='sleep 3 && echo "slept within group {{ params.group_id}} at {{ params.task_id }}"'
             )
             
         with TaskGroup(group_id="process_pool_2") as process_pool_2:
             
             p2_task_1 = BashOperator(
             task_id = "p2_task_1", # similar task id but since its in group, it becomes group_id.task_id
-            bash_command='sleep 3 && echo "slept within group at {{ task_id }}"'
+            bash_command='sleep 3 && echo "slept within group"'
             )
             
             p2_task_2 = BashOperator(
             task_id = "p2_task_2",
-            bash_command='sleep 3 && echo "slept within group {{ group_id}} at {{ task_id }}"'
+            bash_command='sleep 3 && echo "slept within group P2 T2"'
             )
      
-    
-    
-    
-    task_1 >> processing_group 
+    task_1 >> processing_group
